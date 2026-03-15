17a78c78b1b1eef391acf8bf171c4fa06f80d7c1
EL-Masria <EL-Masria@DESKTOP-60USKE8>
2026-03-15 02:15:16 +0200

    fg

--- AgriCureSystem/Migrations/20260315001445_ha.Designer.cs
+++ AgriCureSystem/Migrations/20260315001445_ha.Designer.cs
@@ -4,6 +4,7 @@ using AgriCureSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AgriCureSystem.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260315001445_ha")]
+    partial class ha
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
